7dff74d29c195afa3d755397a131b3aabcfdf781
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2022-11-04 01:55:49 +0600

    scrollspy done

--- script.js
+++ script.js
@@ -6,9 +6,27 @@ menu.addEventListener('click', () => {
   navbar.classList.toggle('active')
 })
 
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header .navbar a')
+
+
 window.addEventListener('scroll', () => {
   menu.classList.remove('fa-times')
   navbar.classList.remove('active')
+
+  sections.forEach(section => {
+    let top = window.scrollY
+    let height = section.offsetHeight
+    let offset = section.offsetTop - 150
+    let id = section.getAttribute('id')
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(nL => {
+        nL.classList.remove('active')
+        document.querySelector(`header .navbar a[href*="${id}"]`).classList.add('active')
+      })
+    }
+  })
 })
 
 document.querySelector('#search-icon').onclick = () => {

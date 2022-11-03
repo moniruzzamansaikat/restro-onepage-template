7a1fa20ca15e07b190f9de9d4645d5f11caf4277
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2022-11-04 01:24:34 +0600

    review section completed

---
+++ README.md
@@ -0,0 +1,3 @@
+# Restro restaurant template
+
+This is a one page template built only with html, css and javascript. It's fully responsive with **flex** and **grid**.
--- script.js
+++ script.js
@@ -32,3 +32,28 @@ let swiper = new Swiper('.home-slider', {
   },
   loop: true
 })
+
+
+let reviewSwiper = new Swiper('.review-slider', {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+})
